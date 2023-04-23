28b2f0b56871cb3ec26ae3dc3df1739f73170247
z3942 <1327735225@qq.com>
2023-04-23 17:43:58 +0800

    deploy, heads/master-0-g99b3d4b

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -38,8 +38,8 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         link: '/yunwei/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
       },
       {
-        text: '编程',
-        link: '/biancheng/',
+        text: '工作',
+        link: '/work/',
       },
       {
         text: '读书笔记',
